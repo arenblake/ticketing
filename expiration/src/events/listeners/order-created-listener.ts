b05da38f1383623f9dfe31f5974cf4e0bc643341
arenblake <freeflowbooking@gmail.com>
2023-01-21 14:27:44 -0600

    expiration worker service added

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,25 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@abticketingapp/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("Millisecond delay is currently:", delay);
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
+
+    msg.ack();
+  }
+}
