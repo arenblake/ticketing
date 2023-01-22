b604106e35162767d8c880868e3899ad3aeec1fc
arenblake <freeflowbooking@gmail.com>
2023-01-22 12:43:47 -0600

    stripe integration complete

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import request from "supertest";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
@@ -5,11 +7,10 @@ import { app } from "../app";
 import jwt from "jsonwebtoken";
 
 declare global {
-  var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
-
-jest.mock('../nats-wrapper')
+jest.mock("../nats-wrapper");
 
 let mongo: any;
 beforeAll(async () => {
@@ -23,7 +24,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
-  jest.clearAllMocks()
+  jest.clearAllMocks();
   const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
@@ -38,9 +39,9 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
