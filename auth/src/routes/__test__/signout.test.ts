7f61a673bb53f55a50af6e6ffcab65aba2529d31
arenblake <freeflowbooking@gmail.com>
2023-01-23 10:34:03 -0600

    test

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("clears the cookie after signing out", async () => {
+  console.log("In a test");
   await request(app)
     .post("/api/users/signup")
     .send({
@@ -10,10 +11,7 @@ it("clears the cookie after signing out", async () => {
     })
     .expect(201);
 
-  const response = await request(app)
-    .post("/api/users/signout")
-    .send({})
-    .expect(200);
+  const response = await request(app).post("/api/users/signout").send({}).expect(200);
 
   expect(response.get("Set-Cookie")[0]).toEqual(
     "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
