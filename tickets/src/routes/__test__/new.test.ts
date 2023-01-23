729f1487829d7c341561b0355c93e8e98136ca3a
arenblake <freeflowbooking@gmail.com>
2023-01-22 18:49:33 -0600

    fixed typo

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -3,7 +3,7 @@ import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { natsWrapper } from "../../nats-wrapper";
 
-it("has a route handler listing to /api/tickets for post requests", async () => {
+it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
   expect(response.status).not.toEqual(404);
 });
@@ -13,10 +13,7 @@ it("can only be accessed if the user is signed in", async () => {
 });
 
 it("returns a status other than 401 if the user is signed in", async () => {
-  const response = await request(app)
-    .post("/api/tickets")
-    .set("Cookie", global.signin())
-    .send({});
+  const response = await request(app).post("/api/tickets").set("Cookie", global.signin()).send({});
   expect(response.status).not.toEqual(401);
 });
 
